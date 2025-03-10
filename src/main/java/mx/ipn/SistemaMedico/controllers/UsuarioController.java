9aa5e9046bf2e9ddc40ba0b14af5aa4c9f502466
albertogzga <albertognzga@gmail.com>
2025-03-09 22:58:49 -0600

    Modificacion

--- src/main/java/mx/ipn/SistemaMedico/controllers/UsuarioController.java
+++
@@ -1,5 +0,0 @@
-package mx.ipn.SistemaMedico.controllers;
-
-public class UsuarioController {
-
-}
---
+++ src/main/java/mx/ipn/SistemaMedico/models/Paciente.java
@@ -0,0 +1,90 @@
+package mx.ipn.SistemaMedico.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pacientes")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "correo", nullable = false, unique = true)
+    private String correo;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "genero")
+    private String genero;
+
+    @Column(name = "imagenPerfil")
+    private String imagenPerfil;
+
+    public Paciente() {}
+
+    // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getImagenPerfil() {
+        return imagenPerfil;
+    }
+
+    public void setImagenPerfil(String imagenPerfil) {
+        this.imagenPerfil = imagenPerfil;
+    }
+}
